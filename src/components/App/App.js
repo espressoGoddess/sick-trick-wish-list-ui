ebbd95ff36f322661591f5857c87cf24a8df1c4c
Amber Shipley <ashipley2@me.com>
2023-04-10 08:35:37 -0700

    Move Trick to proper directory

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,31 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
+import fetchInfo from '../../apiCalls';
+import Trick from '../Trick/Trick';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Sick Trick Wish List</h1>
-      </div>
-    );
+function App() {
+  const [tricks, setTricks] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/v1/tricks')
+      .then(response => response.json())
+      .then(data => setTricks(data))
+  }, []);
+
+  createTricks = () => {
+    if (tricks.length) {
+      const trickCards= tricks.map(trick => {
+        return <Trick />
+      })
+    }
   }
+  return (
+    
+      <main className="App">
+        <h1>Sick Trick Wish List</h1>
+        {trickCards}
+      </main>
+    );
 }
 
 export default App;
