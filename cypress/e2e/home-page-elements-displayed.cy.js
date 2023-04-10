0a28df64795c76fad0caafde361470d664a45e01
Amber Shipley <ashipley2@me.com>
2023-04-10 10:31:44 -0700

    Complete cypress test 1

--- cypress/e2e/home-page-elements-displayed.cy.js
+++ cypress/e2e/home-page-elements-displayed.cy.js
@@ -11,4 +11,26 @@ describe('tricks and form should be displayed when visiting home page', () => {
     cy.get(':nth-child(3) > select')
     cy.get('#tutorial');
   });
+
+  it('should have tricks on page', () => {
+    cy.get('.tricks').children().should('have.length', 3)
+    .get('.tricks > :nth-child(1)')
+    .contains('regular yikes');
+    cy.contains('Obstacle: desk corner');
+    cy.contains('Link to Tutorial:');
+    cy.contains('https://www.javascriptforcats.com');
+
+    cy.get('.tricks > :nth-child(2)')
+    .contains('switch heely');
+    cy.contains('Obstacle: slide');
+    cy.contains('Link to Tutorial:');
+    cy.contains('https://www.javascriptforcats.com');
+
+    cy.get('.tricks > :nth-child(3)')
+    .contains('regular backside beep boop');
+    cy.contains('Obstacle: water fountain');
+    cy.contains('Link to Tutorial:');
+    cy.contains('https://www.javascriptforcats.com');
+
+  });
 })
